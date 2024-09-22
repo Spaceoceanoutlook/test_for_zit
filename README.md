f3ac0f0bfa274a0803b0b1ad827b9b3bc570c713
Spaceocean <spaceocean@outlook.com>
2024-09-22 15:18:37 +0500

    tests are working

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 
 - docker-compose up --build
 - http://127.0.0.1:8000/docs 
+
+## **Тестирование**
+- cd .\tests\
+- команда pytest test_main.py в терминале
--- tests/test_main.py
+++ tests/test_main.py
@@ -10,12 +10,13 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Создание таблиц
-Base.metadata.create_all(bind=engine)
 
-
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", autouse=True)
 def test_db():
+    # Удаление всех таблиц и создание новой базы данных и таблиц
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
     # Создание сессии для тестирования
     db = TestingSessionLocal()
     yield db
